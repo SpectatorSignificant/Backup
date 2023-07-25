fb6363ba659796bf1a5eed4a9bcb26edeae9fbc9
unknown <anishrajan2004@gmail.com>
2023-07-25 22:44:48 +0530

    Update

--- typing-website/client/typing-website/src/Components/TimeDiv.js
+++ typing-website/client/typing-website/src/Components/TimeDiv.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 export default function TimeDiv(){
+    const [elapsedTimeSec, setElapsed] = useState(0);
+
+    const startTime = Date.now();
+    
+    function updateTime(){
+        const currentTime = Date.now();
+        
+        setElapsed(Math.round((currentTime - startTime)/1000));
+    };
+
+    useEffect(() => {  
+        setInterval(updateTime, 50);
+    }, []);
+
     return (
-        <div className="time"></div>
+        <div className="time">{elapsedTimeSec}</div>
     )
 }
